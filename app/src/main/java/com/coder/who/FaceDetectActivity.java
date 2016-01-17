c7ee7b70216d7c1f2f7c2fdf4848647d88d84ea0
JueYingCoder <13141459344@163.com>
2016-01-17 18:17:54 +0800

    完善

--- app/src/main/java/com/coder/who/FaceDetectActivity.java
+++ app/src/main/java/com/coder/who/FaceDetectActivity.java
@@ -7,22 +7,21 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class FaceDetectActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private FaceOverlayView faceOverlayView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_detect);
 
-        imageView = (ImageView) findViewById(R.id.image_face_detect);
+        faceOverlayView = (FaceOverlayView) findViewById(R.id.face_overlay);
         Intent intent =getIntent();
         Bitmap bitmap = BitmapFactory.decodeFile(intent.getStringExtra("FaceImage"));
-        imageView.setImageBitmap(bitmap);
+        faceOverlayView.setBitmap(bitmap,FaceDetectActivity.this);
 
         //进入检测模式
         final SweetAlertDialog pDialog = new SweetAlertDialog(FaceDetectActivity.this, SweetAlertDialog.PROGRESS_TYPE);
@@ -31,8 +30,42 @@ public class FaceDetectActivity extends AppCompatActivity {
         pDialog.setCancelable(false);
         pDialog.show();
 
-        //正儿八经检测逻辑
+        /**
+         * 检测出人脸部分，并进行灰度等预处理
+         *
+         * mFaces.size记录检测出的人脸数
+         *
+         * String[] faceImage存储预处理好的人脸图片
+         */
 
+//       if(Detect_Sucess){
+//
+//
+//           pDialog.dismiss();
+//           /**
+//            * 对检测到人脸是否确认识别，将custom_image改为检测到的图像
+//            *
+//            * 如果检测到多张脸，则用群组头像替代
+//            */
+//           new SweetAlertDialog(FaceDetectActivity.this, SweetAlertDialog.CUSTOM_IMAGE_TYPE)
+//                   .setCustomImage(R.mipmap.icon)
+//                   .setTitleText("已检测到人脸部分")
+//                   .setContentText("为提高准确率请保证网络畅通")
+//                   .setConfirmText("进行识别")
+//                   .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+//                       @Override
+//                       public void onClick(SweetAlertDialog sDialog) {
+//                           Intent i = new Intent(FaceDetectActivity.this,FaceRecognitionActivity.class);
+//                           //传递检测出，并已做了预处理的人脸图片
+//                           i.putExtra("Faces",faceImage);
+//                           startActivity(i);
+//
+//                       }
+//                   })
+//                   .show();
+//       }
+
+        //暂时
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -47,21 +80,9 @@ public class FaceDetectActivity extends AppCompatActivity {
                         .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
                             @Override
                             public void onClick(SweetAlertDialog sDialog) {
-                                sDialog
-                                        .setTitleText("识别成功")
-                                        .setContentText("点击查看具体信息")
-                                        .setConfirmText("OK")
-                                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                                            @Override
-                                            public void onClick(SweetAlertDialog sweetAlertDialog) {
-                                                Intent i = new Intent(FaceDetectActivity.this,FaceRecognitionActivity.class);
-                                                //传递识别结果json
+                                Intent i = new Intent(FaceDetectActivity.this,FaceRecognitionActivity.class);
 
-
-                                                startActivity(i);
-                                            }
-                                        })
-                                        .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                                 startActivity(i);
                             }
                         })
                         .show();
--- app/src/main/java/com/coder/who/FaceRecognitionActivity.java
+++ app/src/main/java/com/coder/who/FaceRecognitionActivity.java
@@ -1,11 +1,15 @@
 package com.coder.who;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class FaceRecognitionActivity extends AppCompatActivity {
 
@@ -25,6 +29,37 @@ public class FaceRecognitionActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /**
+         * 得到传递过来的图片地址，启动网络传输模块上传图片并识别
+         */
+
+//        Intent intent = getIntent();
+//        String images = intent.getStringExtra("Faces");
+
+        //开始识别弹窗
+        final SweetAlertDialog pDialog = new SweetAlertDialog(FaceRecognitionActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        pDialog.setTitleText("识别中");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        //网络模块识别并解析Json文件
+
+
+        final SweetAlertDialog sDialog = new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                pDialog.dismiss();
+
+                //识别成功弹窗
+                sDialog.setTitleText("识别成功");
+                sDialog.setContentText("请勿泄露对方隐私");
+                sDialog.show();
+            }
+        }, 3000);
+
     }
 
 }
